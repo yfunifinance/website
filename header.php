c8f37fa641076a176b517931b4800e60ef3047f8
YFUNI Finance <74447060+yfunifinance@users.noreply.github.com>
2021-03-22 21:33:23 +0700

    Update header.php

--- header.php
+++ header.php
@@ -12,8 +12,9 @@
         <div class="collapse navbar-collapse" id="navcol-1">
             <ul class="nav navbar-nav ml-auto">
                 <li class="nav-item" role="presentation"><a class="nav-link <?php if($menuselected == "home"){ echo "active"; } ?>" href="/">Home</a></li>
-                <li class="nav-item dropdown"><a class="dropdown-toggle nav-link <?php if($menuselected == "platform"){ echo "active"; } ?>" data-toggle="dropdown" aria-expanded="false" href="#">Platform</a>
-                    <div class="dropdown-menu" role="menu">
+                <li class="nav-item dropdown">
+                    <a class="dropdown-toggle nav-link <?php if($menuselected == "platform"){ echo "active"; } ?>" id="#dplatform" data-toggle="dropdown" aria-expanded="false" href="#">Platform</a>
+                    <div class="dropdown-menu" aria-labelled-by="dplatform">
                         <a class="dropdown-item" role="presentation" href="/uEarn">uEarn</a>
                         <a class="dropdown-item" role="presentation" href="/uFarm">uFarm</a>
                         <a class="dropdown-item" role="presentation" href="/uVault">uVault</a>
@@ -22,7 +23,21 @@
                         <a class="dropdown-item" role="presentation" href="/uMortgage">uMortgage</a>
                     </div>
                 </li>
-                <li class="nav-item" role="presentation"><a class="nav-link <?php if($menuselected == "liquidity"){ echo "active"; } ?>" href="/liquidity">Liquidity</a></li>
+                <li class="nav-item dropdown">
+                    <a class="dropdown-toggle nav-link <?php if($menuselected == "liquidity"){ echo "active"; } ?>" id="#dliquidity" data-toggle="dropdown" aria-expanded="false" href="#">Liquidity</a>
+                    <div class="dropdown-menu" aria-labelled-by="dliquidity">
+                        <a target="blank" class="dropdown-item" role="presentation" href="https://exchange.pancakeswap.finance/#/add/BNB/0x7EF548Ee7cA0855B18cC5372BD9A34678da43658">LPYFUNIBNB</a>
+                        <a target="blank" class="dropdown-item" role="presentation" href="https://exchange.pancakeswap.finance/#/add/BNB/0xe3E1FabEaBD48491bD6902B0c32FDEee8D2Ff12b">LPUNICORNBNB</a>
+                        <a target="blank" class="dropdown-item" role="presentation" href="https://exchange.pancakeswap.finance/#/add/BNB/0x31D0a7AdA4d4c131Eb612DB48861211F63e57610">LPSTARTBNB</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="dropdown-toggle nav-link <?php if($menuselected == "liquidity"){ echo "active"; } ?>" id="#dbuy" data-toggle="dropdown" aria-expanded="false" href="#">Buy</a>
+                    <div class="dropdown-menu" aria-labelled-by="dbuy">
+                        <a target="blank" class="dropdown-item" role="presentation" href="https://exchange.pancakeswap.finance/#/swap?inputCurrency=0x7ef548ee7ca0855b18cc5372bd9a34678da43658">YFUNI</a>
+                        <a target="blank" class="dropdown-item" role="presentation" href="https://exchange.pancakeswap.finance/#/swap?inputCurrency=0xe3e1fabeabd48491bd6902b0c32fdeee8d2ff12b">UNICORN</a>
+                    </div>
+                </li>
                 <li class="nav-item" role="presentation"><a class="nav-link" href="https://t.me/yfunifinance">Telegram</a></li>
             </ul>
         </div>
